012803e120fdf22c1f1eea44985cf42945f723e3
Roxanne <roxanne.shoar@gmail.com>
2025-12-14 19:27:08 -0500

    Feat/header: add date and time display & mobile responsiveness

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,28 @@
 import "./header.css";
 import Title from "../../assets/Logo2.png";
+import { useState, useEffect } from "react";
+
 export default function Header() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
     <header className="header">
       <img src={Title} className="header__title" />
+      <p className="header__date-time">
+        {currentTime.toLocaleString()}
+      </p>
     </header>
   );
 }
+
+
