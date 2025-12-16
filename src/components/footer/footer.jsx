42e56278370e92af71bb40fa80a48d11c225bf2c
Roxanne <roxanne.shoar@gmail.com>
2025-12-15 23:31:23 -0500

    Refactor (footer): update text

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footer__text">
-        © 2025 Holiday Mood Tracker | Built with ❤️ by The Elf-gineers (Team 2)
+        © 2025 Holiday Mood Tracker | Built with ❤️ by The Elf-gineers
       </p>
     </footer>
   );
